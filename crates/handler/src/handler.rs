99918f24a403adf79f2132481e1e771469ed4295
Miracle0x <miracle9027@gmail.com>
2025-04-14 08:07:17 -0700

    add pulse for revm

--- crates/handler/src/handler.rs
+++ crates/handler/src/handler.rs
@@ -101,7 +101,7 @@ pub trait Handler {
         let eip7702_refund = self.pre_execution(evm)? as i64;
         let exec_result = self.execution(evm, &init_and_floor_gas)?;
         if evm.ctx().block().number() == 17253037 {
-            println!("Running handler... {:?} chain id {} block number {:?} tx caller {:?} gas price {:?}", evm.ctx().cfg().spec().into(), evm.ctx().cfg().chain_id(), evm.ctx().block().number(), evm.ctx().tx().caller(), evm.ctx().tx().gas_price());
+            println!("Running handler... {:?} chain id {} block number {:?} tx caller {:?} nonce {:?} gas price {:?}", evm.ctx().cfg().spec().into(), evm.ctx().cfg().chain_id(), evm.ctx().block().number(), evm.ctx().tx().caller(), evm.ctx().tx().nonce(), evm.ctx().tx().gas_price());
             println!("Execution result: {:?}", exec_result);
         }
         self.post_execution(evm, exec_result, init_and_floor_gas, eip7702_refund)
